c8342031c4c82af53b62ff22dcbd071f23dbd222
Nadia Kuka PC <nadia.figueroafernandez@epfl.ch>
2016-03-01 13:39:15 +0100

    Using same jointmsg for simulation and realtime.

--- lasa_to_iai_parser/scripts/seperate_to_joint.py
+++ lasa_to_iai_parser/scripts/seperate_to_joint.py
@@ -5,36 +5,34 @@ import rospy
 from rospy.numpy_msg import numpy_msg
 import numpy
 
-from std_msgs.msg           import Float32MultiArray
-from kuka_fri_bridge.msg    import JointStateImpedance
-from iai_control_msgs.msg   import MultiJointVelocityImpedanceCommand
+from sensor.msg             import JointState
 
 def jointStateImpedance_callback(data):
 
     rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
     
 
-class JS_converter(object):
-    """convert kuka_fri_bridge/JointStateImpedance message to std_msgs/Float32MultiArray for position controller simulator"""
+class JS_merger(object):
+    """merge /l_arm/joint_states + /r_arm/joint_states"""
     def __init__(self, num_dof):
         
         self.num_dof = num_dof
         self.init_work_variables()    
-        self.vel_output_topic_type = MultiJointVelocityImpedanceCommand
+        self.output_topic_type = JointState
 
-        self.js_sub = rospy.Subscriber("/l_arm_pos_controller/joint_imp_cmd", numpy_msg(JointStateImpedance), self.js_cb, queue_size=3, tcp_nodelay=True)
-        self.out_js_pos_pub = rospy.Publisher("/l_arm_pos_controller/command", Float32MultiArray, queue_size=3, tcp_nodelay=True, latch=False)
-        self.out_js_vel_pub = rospy.Publisher("/l_arm_vel/command", numpy_msg(self.vel_output_topic_type), queue_size=3, tcp_nodelay=True, latch=False)
+        self.l_js_sub = rospy.Subscriber("/l_arm/joint_states", numpy_msg(JointState), self.l_js_cb, queue_size=1, tcp_nodelay=True)
+        self.r_js_sub = rospy.Subscriber("/l_arm/joint_states", numpy_msg(JointState), self.r_js_cb, queue_size=1, tcp_nodelay=True)
+        self.js_pub   = rospy.Publisher("/joint_states",        numpy_msg(self.output_topic_type), queue_size=1, tcp_nodelay=True, latch=False)
 
     def init_work_variables(self):
         self.js_position = numpy.array([0.0] * self.num_dof, dtype=numpy.float32) 
-                     
+                             
             
     def stop(self):
         '''Stop the object'''
-        self.js_sub.unregister()
-        self.out_js_pos_pub.unregister()
-        self.out_js_vel_pub.unregister()
+        self.l_js_sub.unregister()
+        self.r_js_sub.unregister()        
+        self.out_js_pub.unregister()
         
     def js_cb(self, msg):
         '''Callback function for the joint_states. It searches for the subset of joints, and publishes those on another topic'''
@@ -44,10 +42,6 @@ class JS_converter(object):
         if (numpy.size(msg.position) ==  self.num_dof):
             for i in range(self.num_dof):
                  self.js_position[i] = msg.position[i]
-
-            # rospy.loginfo("\nJoint Position Command [%f,%f,%f,%f,%f,%f,%f]", 
-                # self.js_position[0],self.js_position[1],self.js_position[2],self.js_position[3],
-                # self.js_position[4],self.js_position[5],self.js_position[6])
 
             # Send values of desired joint positions to iai-compatible topic
             
@@ -78,7 +72,7 @@ def main():
     rospy.init_node('lasa_to_iai', anonymous=True)
     rospy.loginfo("%s: Starting" % (rospy.get_name()))
 
-    convert = JS_converter(7)
+    convert = JS_merger(14)
     
     rospy.spin()
     
